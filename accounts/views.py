f29a4c54da973d7012b4047abfeda30d4cc1fe89
seg1129 <geraces@email.chop.edu>
2019-06-25 23:35:31 -0400

    removing IE login notice because tests were failing

--- accounts/views.py
+++ accounts/views.py
@@ -21,7 +21,6 @@ def throttled_login(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
 
-    is_IE = re.findall(r'MSIE', request.META.get('HTTP_USER_AGENT'))
     template_name = 'accounts/login.html'
 
     login_allowed = request.session.get('login_allowed', True)
@@ -39,26 +38,24 @@ def throttled_login(request):
             }
             return render(request, template_name, {
                 'form': form,
-                'is_IE': True,
             })
         except:
             raise
 
         login_allowed = throttle_login(request)
 
-        if login_allowed:
-            response = login(request, template_name=template_name,
-                             authentication_form=BrpAuthenticationForm)
-            # We know if the response is a redirect, the login
-            # was successful, thus we can clear the throttled login counter
-            if isinstance(response, HttpResponseRedirect):
-                request.META['action'] = 'Login successful.'
-                clear_throttled_login(request)
-            return response
+    if login_allowed:
+        response = login(request, template_name=template_name,
+                         authentication_form=BrpAuthenticationForm)
+        # We know if the response is a redirect, the login
+        # was successful, thus we can clear the throttled login counter
+        if isinstance(response, HttpResponseRedirect):
+            request.META['action'] = 'Login successful.'
+            clear_throttled_login(request)
+        return response
 
     return render(request, template_name, {
         'login_not_allowed': not login_allowed,
-        'is_IE': is_IE,
     })
 
 
